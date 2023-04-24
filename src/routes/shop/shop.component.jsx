b04f53e7ab6ac66045de74b2075fe10bc3fe3ff4
Jay Jernigan <jjernigan1065@gmail.com>
2023-04-24 14:07:01 -0400

    finished up category page, category preview, shop routes for categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,33 +1,24 @@
-import { Fragment, useContext } from 'react'
-import { Link } from 'react-router-dom'
-
-import ProductCard from '../../components/product-card/product-card.component'
+import { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 import { CategoriesContext } from '../../contexts/categories.context'
+import CategoryPreviews from '../category-preview/category-preview.component'
+import Category from '../category/category.component'
 
 import './shop.styles.scss'
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext)
-
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map(title => (
-        <Fragment key={title}>
-          <Link to={`/shop/${title}`}>
-            <h2>{title}</h2>
-          </Link>
-          <div className='products-container'>
-            {categoriesMap[title].slice(0, 4).map(product => (
-              <ProductCard
-                key={product.id}
-                product={product}
-              />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Routes>
+      <Route
+        index
+        element={<CategoryPreviews />}
+      />
+      <Route
+        path=':category'
+        element={<Category />}
+      />
+    </Routes>
   )
 }
 
