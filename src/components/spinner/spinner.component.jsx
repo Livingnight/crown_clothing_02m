560cbc5e3ea9276af8cac1c47e2e02262114d4b5
Jay Jernigan <jjernigan1065@gmail.com>
2023-05-10 15:28:54 -0400

    added in a loading spinner using start, success, and failure thunks using redux-thunks

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,9 @@
+const { SpinnerContainer, SpinnerOverlay } = require('./spinner.styles')
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+)
+
+export default Spinner
